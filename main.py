117165d52d7cd907cdaf3d90f252681587546347
kaykoe <karolkoenigg@gmail.com>
2025-03-14 21:44:51 +0100

    fix exception handling, add tabs, prepare for embedding .txt files into .exe

--- main.py
+++ main.py
@@ -1,11 +1,91 @@
 import io
-import os
+from os import path
 import pandas as pd
 import numpy as np
 import FreeSimpleGUI as sg
 from matplotlib import pyplot as plt
+import typing
 
-# TODO: split up into more files, this is getting messy
+
+bundle_path: str = path.abspath(path.dirname(__file__))
+default_turnaround_time_file = path.join(bundle_path, "czas.txt")
+turnaround_time_file = default_turnaround_time_file
+default_intensity_time_file = path.join(bundle_path, "int.txt")
+intensity_time_file = default_intensity_time_file
+
+size = (1280, 720)
+current_tab: str = "wizualizacja"
+
+window: sg.Window
+tab_img_dict: typing.Dict[str, str]
+
+
+def setup():
+    sg.theme("LightGrey")
+
+    menu_layout = [
+        ["Plik", ["Otwórz", "Zamknij"]],
+        ["Pomoc", ["O programie"]],
+    ]
+
+    tab_1_layout = [
+        [sg.VPush()],
+        [sg.Push(), sg.Image(key="wizualizacja_image", size=size), sg.Push()],
+        [sg.VPush()],
+    ]
+    # TODO: add more tabs
+    layout = [
+        [sg.Menu(menu_layout)],
+        [
+            sg.TabGroup(
+                [[sg.Tab("Wizualizacja", tab_1_layout, key="wizualizacja")]],
+                expand_x=True,
+                expand_y=True,
+                enable_events=True,
+            )
+        ],
+    ]
+    global tab_img_dict
+    tab_img_dict = {"wizualizacja": "wizualizacja_image"}
+
+    global window
+    window = sg.Window(
+        "Wizualizacja GNR",
+        layout,
+        resizable=True,
+        finalize=True,
+        element_justification="center",
+    )
+
+
+def get_filenames_from_popup(vals: typing.Dict[str, str]) -> (
+    str,
+    str,
+) or None:
+    def is_valid_file(file_path: str) -> bool:
+        return path.isfile(file_path) and file_path.endswith(".txt")
+
+    not_selected: bool = (
+        not vals["int_file"]
+        or not vals["time_file"]
+        or vals["int_file"] == ""
+        or vals["time_file"] == ""
+    )
+    if not_selected:
+        sg.popup_error("Nie wybrano pliku/plików")
+        return None
+
+    if not is_valid_file(vals["time_file"]):
+        sg.popup_error(
+            "Niepoprawna ścieżka do pliku z czasami obsługi, upewnij się że plik istnieje i jest w formacie .txt"
+        )
+        return None
+    if not is_valid_file(vals["int_file"]):
+        sg.popup_error(
+            "Niepoprawna ścieżka do pliku z intensywnością zgłoszeń, upewnij się że plik istnieje i jest w formacie .txt"
+        )
+        return None
+    return vals["time_file"], vals["int_file"]
 
 
 def get_load_df(
@@ -17,13 +97,13 @@ def get_load_df(
     The load column is calculated as the product of the intensity and the average turnaround time.
     """
 
-    def calculate_average_turnaround_time(path: str) -> float | None:
+    def calculate_average_turnaround_time(file_path: str) -> float | None:
         """
         This function takes in a path to a file containing turnaround times and returns the average turnaround time.
         The file must be formated in a way where the times are provided one per line and nothing else.
         """
         try:
-            df = pd.read_table(path, header=None, index_col=False)
+            df = pd.read_table(file_path, header=None, index_col=False)
             return df.mean()[0]
         except Exception:
             sg.popup_error(
@@ -31,20 +111,21 @@ def get_load_df(
             )
             return None
 
-    def get_request_intensity_df(path: str) -> pd.DataFrame | None:
+    def get_request_intensity_df(file_path: str) -> pd.DataFrame | None:
         """
         This function takes in a path to a file containing request intensities and returns a DataFrame with the minute and intensity columns.
         The file must be formated in a way where the minute and intensity are provided in two columns separated by whitespace.
         """
         try:
-            return pd.read_table(
-                path,
+            df = pd.read_table(
+                file_path,
                 decimal=",",
                 header=None,
                 sep="\s+",
                 names=["minute", "intensity"],
                 dtype={"minute": np.int32, "intensity": np.float32},
             )
+            return df
         except Exception:
             sg.popup_error(
                 "Błąd podczas przetwarzania pliku z intensywnością zgłoszeń, niepoprawny format"
@@ -54,24 +135,29 @@ def get_load_df(
     avg_ta = calculate_average_turnaround_time(turnaround_time_file_path)
     # Go back to the default file if there was an error
     if avg_ta is None:
-        turnaround_time_file = "czas.txt"
+        global turnaround_time_file
+        turnaround_time_file = default_turnaround_time_file
         avg_ta = calculate_average_turnaround_time(turnaround_time_file)
 
     # Go back to the default file if there was an error
     req_df = get_request_intensity_df(intensity_time_file_path)
     if req_df is None:
-        intensity_time_file = "int.txt"
+        global intensity_time_file
+        intensity_time_file = default_intensity_time_file
         req_df = get_request_intensity_df(intensity_time_file)
+    # TODO: find out if this is what we are supposed to do with this data and if there are some other things that I am supposed to do with the data before using it
+    # TODO: find out if I am supposed to add data where the minutes are missing
     return req_df.assign(load=req_df["intensity"] * avg_ta)
 
 
 def get_GNR(load_df: pd.DataFrame) -> pd.DataFrame:
-    # TODO: implement this
+    # TODO: implement this, if multiple ways of calculating, just get it from the tab dict
     pass
 
 
 def get_plot(load_df: pd.DataFrame) -> plt.Figure:
     # TODO: add GNR to the plot
+    # TODO: make the plot look better etc.
     plot = load_df.plot(
         x="minute",
         y="load",
@@ -83,49 +169,18 @@ def get_plot(load_df: pd.DataFrame) -> plt.Figure:
     return plot
 
 
-def update_image():
+def update_image(key: str):
     load_df = get_load_df(turnaround_time_file, intensity_time_file)
     plot = get_plot(load_df)
     img = io.BytesIO()
     plot.savefig(img, format="png")
-    window["image"].update(data=img.getvalue())
+    window[key].update(data=img.getvalue())
 
 
-def is_valid_file(path: str) -> bool:
-    return os.path.isfile(path) and path.endswith(".txt")
-
-
-# TODO: embed default files into exe
-turnaround_time_file = "czas.txt"
-intensity_time_file = "int.txt"
-size = (1280, 720)
-sg.theme("LightGrey")
-
-# TODO: add descriptions for different ways of calculating GNR to the help menu
-menu_layout = [
-    ["Plik", ["Otwórz", "Zamknij"]],
-    [
-        "Informacje",
-        ["O programie", "Sposoby obliczania GNR", ["Wzór 1", "Wzór 2", "Wzór 3"]],
-    ],
-]
-
-layout = [
-    [sg.Menu(menu_layout)],
-    [sg.VPush()],
-    [sg.Push(), sg.Image(key="image", size=size), sg.Push()],
-    [sg.VPush()],
-]
-window = sg.Window(
-    "Wizualizacja GNR",
-    layout,
-    resizable=True,
-    finalize=True,
-    element_justification="center",
-)
-
-update_image()
+setup()
+update_image(tab_img_dict[current_tab])
 window.refresh()
+
 while True:
     event, values = window.read()
 
@@ -140,7 +195,7 @@ while True:
                 sg.FileBrowse(file_types=(("Text Files", "*.txt"),)),
             ],
             [
-                sg.Text("Plik z intesywnością zgłoszeń"),
+                sg.Text("Plik z intensywnością zgłoszeń"),
                 sg.Input(key="int_file"),
                 sg.FileBrowse(file_types=(("Text Files", "*.txt"),)),
             ],
@@ -151,47 +206,31 @@ while True:
                 sg.Push(),
             ],
         ]
-
         event, values = sg.Window(
             "Wybierz dane wejściowe",
             popup_layout,
-            size=(610, 120),
             element_justification="right",
+            keep_on_top=True,
         ).read(close=True)
 
-        if event == "Submit":
-            not_selected: bool = (
-                not values["Submit"]
-                or (
-                    not values["Submit"]["int_file"]
-                    or not values["Submit"]["time_file"]
-                )
-                or (
-                    values["Submit"]["int_file"] == ""
-                    or values["Submit"]["time_file"] == ""
-                )
-            )
-            if not_selected:
-                sg.popup_error("Nie wybrano pliku/plików")
+        if event == "Zatwierdź":
+            filenames = get_filenames_from_popup(values)
+            if filenames is None:
                 continue
+            turnaround_time_file, intensity_time_file = filenames
 
-            if not is_valid_file(values["Submit"]["time_file"]):
-                sg.popup_error(
-                    "Niepoprawna ścieżka do pliku z czasami obsługi, upewnij się że plik istnieje i jest w formacie .txt"
-                )
-                continue
-            if not is_valid_file(values["Submit"]["int_file"]):
-                sg.popup_error(
-                    "Niepoprawna ścieżka do pliku z intensywnością zgłoszeń, upewnij się że plik istnieje i jest w formacie .txt"
-                )
-                continue
-            turnaround_time_file = values["Submit"]["time_file"]
-            intensity_time_file = values["Submit"]["int_file"]
-        update_image()
+        if tab_img_dict.get(current_tab) is not None:
+            update_image(tab_img_dict[current_tab])
         continue
 
     if event == "Zamknij":
-        turnaround_time_file = "czas.txt"
-        intensity_time_file = "int.txt"
-        update_image()
+        turnaround_time_file = default_turnaround_time_file
+        intensity_time_file = default_intensity_time_file
+
+        if tab_img_dict.get(current_tab) is not None:
+            update_image(tab_img_dict[current_tab])
         continue
+
+    if tab_img_dict.get(event) is not None:
+        current_tab = event
+        update_image(tab_img_dict[current_tab])
